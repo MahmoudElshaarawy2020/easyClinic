80965e64a93251dac5c313d5884d7540fcb36483
Mahmoud ELshaarawy <MahmoudFarag1842001@gmail.com>
2023-11-10 17:52:21 +0200

    UI implementaion

---
+++ app/src/main/java/com/example/clinic/ImageCardFunc.kt
@@ -0,0 +1,30 @@
+package com.example.clinic
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LocalImage(painter: Painter, imageSize: Dp, padding : Dp) {
+    Image(
+        painter = painter,
+        contentDescription = null, // Provide a meaningful description if needed
+        modifier = Modifier
+            .size(imageSize)
+            .padding(padding)
+    )
+}
+
