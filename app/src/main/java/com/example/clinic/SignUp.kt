6f61cb5544405700f37400689e70c2297335f375
fauuomy <melfauomy902@gmail.com>
2023-11-13 14:04:10 -0800

    sign up finished

--- app/src/main/java/com/example/clinic/SignUp.kt
+++ app/src/main/java/com/example/clinic/SignUp.kt
@@ -4,15 +4,43 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RegisterScreenTextFields(){
     Column(
@@ -30,11 +58,102 @@ fun RegisterScreenTextFields(){
             padding = 30.dp,
         )
 
+        Text(
+            text = "SIGN UP",
+            fontSize = 30.sp,
+            color = colorResource(R.color.lightblue)
 
+        )
+        var text by remember { mutableStateOf(TextFieldValue("")) }
+
+        OutlinedTextField(modifier = Modifier
+            .padding(start = 13.dp, end = 13.dp, top = 25.dp)
+            .fillMaxWidth(),
+            value = text, onValueChange = { text = it },
+            shape = RoundedCornerShape(15.dp),
+            label = {
+                Text(
+                    text = "User Name",
+                    color = Color.LightGray
+                )
+            }, leadingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Person, contentDescription = "Email icon"
+                )
+
+            },
+            singleLine = true,
+            textStyle = TextStyle(
+                color = Color.Blue,
+                fontWeight = FontWeight.Bold
+            ),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                unfocusedBorderColor = colorResource(id = R.color.lightblue),
+                focusedBorderColor = colorResource(id = R.color.lightblue)
+            )
+        )
+        var passwordVisability by remember { mutableStateOf(false) }
+        val icon = if (passwordVisability)
+            painterResource(id = R.drawable.visabilityon)
+        else
+            painterResource(id = R.drawable.visabilityoff)
+        var password by remember { mutableStateOf(TextFieldValue("")) }
+        OutlinedTextField(
+            modifier = Modifier
+                .padding(start = 13.dp, end = 13.dp, top = 15.dp)
+                .fillMaxWidth(),
+            value = password,
+            onValueChange = { password = it },
+            visualTransformation = if (passwordVisability) VisualTransformation.None
+            else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            shape = RoundedCornerShape(15.dp),
+
+            label = { Text(text = "password", color = Color.LightGray) },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Lock, contentDescription = "Email icon"
+                )
+            },
+            trailingIcon = {
+                IconButton(onClick = { passwordVisability = !passwordVisability }) {
+                    Icon(
+                        modifier = Modifier
+                            .padding(end = 10.dp)
+                            .size(20.dp), painter = icon, contentDescription = null
+                    )
+
+
+                }
+            },
+            singleLine = true,
+            textStyle = TextStyle(
+                color = Color.Blue,
+                fontWeight = FontWeight.Bold
+            ),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                unfocusedBorderColor = colorResource(id = R.color.lightblue),
+                focusedBorderColor = colorResource(id = R.color.lightblue)
+            )
+        )
+        Button(
+            onClick = {}, modifier = Modifier
+                .padding(start = 30.dp, end = 30.dp, top = 40.dp)
+                .size(height = 40.dp, width = 400.dp),
+            colors = ButtonDefaults.buttonColors(colorResource(id = R.color.lightblue)),
+
+            ) {
+            Text(
+                text = "Sign up",
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+        }
     }
 }
-@Preview(showBackground = true)
-@Composable
-fun signupPreview(){
-    RegisterScreenTextFields()
-}
+    @Preview(showBackground = true)
+    @Composable
+    fun signupPreview() {
+        RegisterScreenTextFields()
+    }
