f4e20628804dc7be9660d228b6aa395614bd7951
Mahmoud ELshaarawy <MahmoudFarag1842001@gmail.com>
2023-12-10 17:54:47 +0200

    patient home finished

--- app/src/main/java/views/MainActivity.kt
+++ app/src/main/java/views/MainActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.clinic.R
 import com.example.clinic.navigation.Navigation
 import com.example.clinic.splashScreenStuff.MainViewModel
 
@@ -15,7 +19,11 @@ class MainActivity : ComponentActivity() {
     private val viewModel : MainViewModel by viewModels()
     lateinit var navController : NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+        val fontFamily = FontFamily(
+            Font(R.font.audiowideregular, FontWeight.Thin)
+        )
         installSplashScreen().apply {
             setKeepVisibleCondition{
                 viewModel.isLoading.value
